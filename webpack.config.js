e27832df2595d61a22e66043421cf703203957a2
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-01 16:55:08 -0500

    add build configuration

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
+
+module.exports = env => {
+
+  const config = {
+    entry: [
+      './src/index.js'
+    ],
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          loaders: [
+            'babel-loader?sourceMap'
+          ],
+          test: /\.js$/
+        }
+      ]
+    },
+    output: {
+      filename: 'index.js',
+      path: `${__dirname}/dist`
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: './src/index.html'
+      })
+    ]
+  };
+
+  if (env && env.dev) {
+
+    config.entry.unshift('webpack-dev-server/client?http://localhost:8080/');
+    config.plugins.unshift(new Webpack.HotModuleReplacementPlugin());
+
+    config.devtool = 'source-map';
+
+    config.devServer = {
+      contentBase: './',
+      hot: true
+    };
+
+  }
+
+  return config;
+
+};
