9e119591260d7f1296dd8fb5874f2c5127e6b90d
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-04 21:35:32 -0500

    add analytics

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ document.querySelector('main#content').innerHTML = `
   ${require('./components/header.html')}
   ${require('inlinedb-docs')}
   ${require('./components/footer.html')}
+  ${require('./components/analytics.html')}
 `;
